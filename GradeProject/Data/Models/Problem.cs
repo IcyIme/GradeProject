3280424e5f266964114cacda0befafd61e45098e
icyime <peter.odrobinak@outlook.sk>
2024-06-20 21:43:09 +0200

    Leaderboard.razor

--- GradeProject/Data/Models/Problem.cs
+++ GradeProject/Data/Models/Problem.cs
@@ -6,5 +6,12 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public string Difficulty { get; set; }
+        public string InitialCode { get; set; }
+        public Resultse[] Result { get; set; }
     }
+}
+
+public class Resultse
+{
+    public string[] result { get; set; }
 }
